7e649a23cae7893e8dc20682eb47e204522d22c5
Vamsi12145 <polepallebharathwaj8586@gmail.com>
2025-05-03 18:19:37 +0530

    sql

--- RegularExpressions.py
+++ RegularExpressions.py
@@ -30,6 +30,6 @@ Tesla's gross cost of operating lease vehicles in FY2021 Q1 was $4.85 billion.
 BMW's gross cost of operating vehicles in FY2021 S1 was $8 billion.
 '''
 
-pattern='FY(\d{4} (?:Q[1-4]|S[1-2]))'
+pattern='FY(\d{4} (?:Q[1-4]|S[1-2]))' #here \d - is for filtering digits and ?: using non capture groups [] setof groups,() capturing of groups.
 matches=re.findall(pattern,tesla)
 print(matches)
