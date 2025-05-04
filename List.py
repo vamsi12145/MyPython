b70a61dd1f29aa1f858516ac92071e3e7655c1fd
Vamsi12145 <polepallebharathwaj8586@gmail.com>
2025-05-04 05:36:46 +0530

    Loops and Lists

---
+++ List.py
@@ -0,0 +1,12 @@
+myList=[12,"vamsi",98.76,]
+
+print(myList[0])
+print(myList[1])
+myList[2]= "Krishna"
+print(myList)
+myList.append("Learn Python")
+print(myList)
+myList1=["Become a DataEngineer"]
+List=myList+myList1
+print(List)
+"Krishna" in  List
---
+++ Loops.py
@@ -0,0 +1,8 @@
+Technology= str(input("Enter the Technology which you have experience"))
+Technology1= str(input("Enter the Second Technology which you have experience"))
+if Technology =="Tibco":
+    print("You areone in Million")
+elif Technology1 != "SpringBoot":
+        print("Please Upskill This Year Otherwise you will lose Your Carrer")
+else:
+        print("Try to Learn SpringBoot or DataEngineer")
